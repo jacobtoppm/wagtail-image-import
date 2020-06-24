9b2653efa73a92699a92626a8045fb34595f735f
jacobtoppm <jacobtm@torchbox.com>
2020-06-24 09:58:00 +0100

    Use register_admin_urls hook to include import urls, also ensuring that not logged in users get redirected

--- wagtail_image_import/wagtail_hooks.py
+++ wagtail_image_import/wagtail_hooks.py
@@ -1,7 +1,10 @@
+from django.conf.urls import include, url
 from django.contrib.auth.models import Permission
 
 from wagtail.core import hooks
 from wagtail.images import get_image_model_string
+
+import wagtail_image_import.urls as image_import_urls
 
 
 @hooks.register("register_permissions")
@@ -10,3 +13,10 @@ def register_import_permission():
         content_type__app_label=get_image_model_string().split(".")[0],
         codename__in=["import_image"],
     )
+
+
+@hooks.register('register_admin_urls')
+def register_import_urls():
+  return [
+    url(r'^image-import/', include(image_import_urls)),
+  ]
