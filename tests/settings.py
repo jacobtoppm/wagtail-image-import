de9b2e25352ecc0e696cd4e7f47d92efa9f88ecf
Jacob Topp-Mugglestone <55137073+jacobtoppm@users.noreply.github.com>
2020-12-04 14:24:47 +0000

    Remove SiteMiddleware from test settings

--- tests/settings.py
+++ tests/settings.py
@@ -41,7 +41,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
